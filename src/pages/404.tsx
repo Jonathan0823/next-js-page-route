f0f426ad7bf3e45b61e897567f32188dfe40c65d
Guinn <eguinjonathan08@gmail.com>
2024-09-24 20:27:29 +0700

    custom 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Custom404 = () => {
+  return (
+    <div>Halaman tidak ditemukan</div>
+  )
+}
+
+export default Custom404
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
-
+import Head from "next/head";
 export default function Home() {
   return (
     <>
+    <Head>
+      <title>Home</title>
+    </Head>
      <div>
       Hello World
      </div>
